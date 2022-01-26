a21dce2ecac316e03683ce3679b3f0bebde316c6
Anastasia <aabogat92@mail.ru>
2022-01-26 16:39:47 +0300

    выполнен PP + изменен принцип отображения новостей

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector('.menu');
 const buttonOpen = document.querySelector('.header__button-open');
 const buttonClose = document.querySelector('.menu__button-close');
-const newsCard = document.querySelectorAll('.news__card');
+const newsCards = document.querySelectorAll('.news__card');
+const newsBackButton = document.querySelector('.news__button--back');
+const newsForwardButton = document.querySelector('.news__button--forward');
+const newsContainer = document.querySelector('.news__card-wrapper');
+
 
 //меню
 
@@ -16,47 +20,30 @@ buttonClose.addEventListener('click', () => {
 })
 
 // видимый список новостей
+let startTranslate = 0;
 
-const checkMedia = () => {
-
-    const desktop = window.matchMedia('(min-width: 1920px)');
-    const tablet = window.matchMedia('(min-width: 768px)');
-
-
-    if(desktop.matches) {
-        for (let i = 0; i < newsCard.length; i++)
-        newsCard[i].classList.add('news__card--visible');
-    }
+const scrollNews = () => {
     
-    else if(tablet.matches) {
-        newsCard[0].classList.add('news__card--visible');
-        newsCard[1].classList.add('news__card--visible');
-    }
     
-    else {
-        newsCard[0].classList.add('news__card--visible');
-    }
+    let newWidth = document.querySelector('.news__image').offsetWidth;
+    
+    newsForwardButton.addEventListener('click', () => {
+        if(startTranslate <= newsContainer.offsetWidth) {
+            startTranslate += (newWidth + 20);
+            for (let i = 0; i < newsCards.length; i++)
+            newsCards[i].style.transform = `translate(${'-' + startTranslate +'px'})`; 
+        }
+    });
+
+    newsBackButton.addEventListener('click', () => {
+        if(startTranslate > 0) {
+            startTranslate -= (newWidth + 20);
+            for (let i = 0; i < newsCards.length; i++)
+            newsCards[i].style.transform = `translate(${'-' +  startTranslate + 'px'})`; 
+        }
+    });
 }
 
 
-window.addEventListener('resize', () => {
-    if(window.innerWidth < 768) {
-        for (let i = 0; i < newsCard.length; i++)
-        newsCard[i].classList.remove('news__card--visible');
-        newsCard[0].classList.add('news__card--visible');
-    }
 
-    else if(window.innerWidth >= 768 && window.innerWidth < 1920) {
-        for (let i = 0; i < newsCard.length; i++)
-        newsCard[i].classList.remove('news__card--visible');
-        newsCard[0].classList.add('news__card--visible');
-        newsCard[1].classList.add('news__card--visible');
-    }
-
-    else {
-        for (let i = 0; i < newsCard.length; i++)
-        newsCard[i].classList.add('news__card--visible');
-    }
-})
-
-checkMedia();
+scrollNews();
