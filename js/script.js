68f7758220ef38dfa3def76e046a0a8dfbb0db52
Anastasia <aabogat92@mail.ru>
2022-02-07 15:49:07 +0300

    добавлены исправления

--- js/script.js
+++ js/script.js
@@ -1,49 +1,31 @@
-const menu = document.querySelector('.menu');
-const buttonOpen = document.querySelector('.header__button-open');
-const buttonClose = document.querySelector('.menu__button-close');
-const newsCards = document.querySelectorAll('.news__card');
-const newsBackButton = document.querySelector('.news__button--back');
-const newsForwardButton = document.querySelector('.news__button--forward');
-const newsContainer = document.querySelector('.news__card-wrapper');
 
+
+const swiper = new Swiper('.js-swiper', {
+    slidesPerView: 1,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+swiper;
+
+const menu = $('.js-menu');
+const buttonOpen = $('.js-header-button-open');
+const buttonClose = $('.js-menu-button-close');
 
 //меню
 
-menu.classList.remove('menu--nojs');
+menu.removeClass('js-menu-nojs');
 
-buttonOpen.addEventListener('click', () => {
-    menu.classList.toggle('menu--opened');
+buttonOpen.on('click', function() {
+    menu.toggleClass('js-menu-opened')
 });
 
-buttonClose.addEventListener('click', () => {
-    menu.classList.toggle('menu--opened');
-})
-
-// видимый список новостей
-let startTranslate = 0;
-
-const scrollNews = () => {
-    
-    
-    let newWidth = document.querySelector('.news__image').offsetWidth;
-    
-    newsForwardButton.addEventListener('click', () => {
-        if(startTranslate <= newsContainer.offsetWidth) {
-            startTranslate += (newWidth + 20);
-            for (let i = 0; i < newsCards.length; i++)
-            newsCards[i].style.transform = `translate(${'-' + startTranslate +'px'})`; 
-        }
-    });
-
-    newsBackButton.addEventListener('click', () => {
-        if(startTranslate > 0) {
-            startTranslate -= (newWidth + 20);
-            for (let i = 0; i < newsCards.length; i++)
-            newsCards[i].style.transform = `translate(${'-' +  startTranslate + 'px'})`; 
-        }
-    });
-}
+buttonClose.on('click', function() {
+    menu.toggleClass('js-menu-opened');
+});
 
 
 
-scrollNews();
